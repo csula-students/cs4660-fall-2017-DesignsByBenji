effca4818e63d3c32beaa5205df3a35a94db11b8
Benjamin Jennings <jennings.benji@gmail.com>
2017-08-25 21:14:58 -0700

    changed references from math to maths

--- cs4660/test/test_math.py
+++ cs4660/test/test_math.py
@@ -2,17 +2,17 @@
 
 import unittest
 
-from math import math
+from maths import maths
 
 class MathTestCase(unittest.TestCase):
     """MathTestCase defines test cases for math module"""
     def test_add_method(self):
         """test_add_method simply test the add method"""
-        self.assertEqual(3, math.add(1, 2))
-        self.assertEqual(6, math.add(3, 3))
+        self.assertEqual(3, maths.add(1, 2))
+        self.assertEqual(6, maths.add(3, 3))
 
     def test_multiply_method(self):
         """test multiply result"""
-        self.assertEqual(6, math.multiply(2, 3))
-        self.assertEqual(8, math.multiply(2, 4))
-        self.assertEqual(8, math.multiply(4, 2))
+        self.assertEqual(6, maths.multiply(2, 3))
+        self.assertEqual(8, maths.multiply(2, 4))
+        self.assertEqual(8, maths.multiply(4, 2))
