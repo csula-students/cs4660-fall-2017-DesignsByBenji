b62daab3e7f5a555304b515c2802b6a8604dcfd1
Benjamin Jennings <jennings.benji@gmail.com>
2017-09-02 09:49:54 -0700

    read file from path

--- cs4660/tutorial/files.py
+++ cs4660/tutorial/files.py
@@ -1,5 +1,5 @@
 """Files tests simple file read related operations"""
-
+from io import open
 class SimpleFile(object):
     """SimpleFile tests using file read api to do some simple math"""
     def __init__(self, file_path):
@@ -8,6 +8,8 @@ class SimpleFile(object):
         TODO: reads the file by path and parse content into two
         dimension array (numbers)
         """
+        f = open(file_path, encoding='utf-8')
+        text = f.read()
 
     def get_mean(self, line_number):
         """
