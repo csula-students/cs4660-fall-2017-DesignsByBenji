9711e7737bb0fe729f211fa77fd1404f756240b3
Benjamin Jennings <jennings.benji@gmail.com>
2017-09-02 11:56:12 -0700

    completed functions for exercise2

--- cs4660/tutorial/files.py
+++ cs4660/tutorial/files.py
@@ -8,33 +8,42 @@ class SimpleFile(object):
         TODO: reads the file by path and parse content into two
         dimension array (numbers)
         """
-        f = open(file_path, encoding='utf-8')
-        text = f.read()
+        f = open(file_path)
+        for line in f:
+            self.numbers.append([float(i) for i in line.strip().split(' ')])
+        f.close()
 
     def get_mean(self, line_number):
         """
         get_mean retrieves the mean value of the list by line_number (starts
         with zero)
         """
-        pass
+        avg = 0
+        for i in self.numbers[line_number]:
+            avg += i
+        avg = avg/len(self.numbers[line_number])
+        return avg
 
     def get_max(self, line_number):
         """
         get_max retrieves the maximum value of the list by line_number (starts
         with zero)
         """
-        pass
+        return max(self.numbers[line_number])
 
     def get_min(self, line_number):
         """
         get_min retrieves the minimum value of the list by line_number (starts
         with zero)
         """
-        pass
+        return min(self.numbers[line_number])
 
     def get_sum(self, line_number):
         """
         get_sum retrieves the sumation of the list by line_number (starts with
         zero)
         """
-        pass
+        sum = 0
+        for i in self.numbers[line_number]:
+            sum += i
+        return sum
